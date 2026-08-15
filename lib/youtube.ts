49ea7192978238b612792557143a1327c5bb90dd
adilweee <adilerkamtek2011@gmail.com>
2026-08-15 14:44:48 +0300

    Correct YouTube thumbnail aspect ratio

--- lib/youtube.ts
+++ lib/youtube.ts
@@ -6,6 +6,6 @@ export function youtubeThumbnail(url?: string | null) {
     if (!id && parsed.hostname.includes('youtu.be')) id = parsed.pathname.split('/').filter(Boolean)[0];
     if (!id && parsed.pathname.startsWith('/shorts/')) id = parsed.pathname.split('/')[2];
     if (!id && parsed.pathname.startsWith('/embed/')) id = parsed.pathname.split('/')[2];
-    return id && /^[\w-]{11}$/.test(id) ? `https://i.ytimg.com/vi/${id}/hqdefault.jpg` : null;
+    return id && /^[\w-]{11}$/.test(id) ? `https://i.ytimg.com/vi/${id}/mqdefault.jpg` : null;
   } catch { return null; }
 }
